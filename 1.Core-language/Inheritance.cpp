c51a1cf3a16664c111caa50f6c838173e14e74f3
Tejas Pawar <pawartejas2307@gmail.com>
2026-07-31 22:44:05 +0530

    feat: Learn about Inheritance

---
+++ 1.Core-language/Inheritance.cpp
@@ -0,0 +1,29 @@
+// Inheritance 
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Person {
+public:
+    string name;
+    int age;
+};
+
+class Student : public Person {
+public:
+    int rollno;
+};
+
+class GradStudent : public Student {
+public:
+    string researchArea;
+};
+
+int main() {
+    GradStudent s1;
+    s1.name = "Tejas";
+    s1.researchArea = "Quantam Physcis";
+    cout << s1.name << endl;
+    cout << s1.researchArea << endl;
+}
